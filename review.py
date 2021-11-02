ba5b8c841ce4719839f9cd276e98e0510e891176
Yanhong Sha <yanhongsha333@gmail.com>
2021-11-02 17:45:31 +0100

    add filter function

--- review.py
+++ review.py
@@ -2,11 +2,18 @@
 # % 用于求余数
 data = []
 count = 0
-
+x = 0 
+l = 0
+sum = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
-		count += 1
-		if count % 1000 == 0:
-		    print(len(data))
+		count += 1	
+print('Fully read, the number is', len(data), 'cases')
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print(len(new))
+print(new[0])
